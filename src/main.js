09612b68b85098d0b9f6c04af978e97845088c36
royc67 <royc67@gmail.com>
2020-07-20 12:55:24 +0300

    Timestamp creation

--- src/main.js
+++ src/main.js
@@ -11,3 +11,17 @@ function addItem(){
 
     input.focus(); input.value = ''; 
 }
+
+// creating timestamp for task:
+function getTime() {
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth()+1; 
+    let yyyy = today.getFullYear();
+    let hh = today.getHours();
+    let min = today.getMinutes();
+    if(dd<10) dd='0'+dd;
+    if(mm<10) mm='0'+mm;
+    today = dd+'/'+mm+'/'+yyyy+" "+hh+":"+min;
+    return today; 
+}
