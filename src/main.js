b84a2162a6fbd1c0d3bbfc442d4baaad0f443403
royc67 <royc67@gmail.com>
2020-07-20 12:54:17 +0300

    add Item func

---
+++ src/main.js
@@ -0,0 +1,13 @@
+
+
+function addItem(){
+    let input = document.getElementById('textInput');
+    let prior = document.getElementById('prioritySelector');
+    let prior = document.getElementById('prioritySelector');
+    
+
+
+
+
+    input.focus(); input.value = ''; 
+}
