aca2964c62ad761c535ec2daddfa69ced86152d0
royc67 <royc67@gmail.com>
2020-07-20 13:43:10 +0300

    minor fixes

--- src/main.js
+++ src/main.js
@@ -3,12 +3,9 @@
 function addItem(){
     let input = document.getElementById('textInput');
     let prior = document.getElementById('prioritySelector');
-    let prior = document.getElementById('prioritySelector');
-    
-
-
-
-
+    let viewSec = document.getElementById('viewSection');
+    let newContainer = createContainer(input.value, prior.value);
+    viewSec.appendChild(newContainer);
     input.focus(); input.value = ''; 
 }
 
@@ -20,8 +17,29 @@ function getTime() {
     let yyyy = today.getFullYear();
     let hh = today.getHours();
     let min = today.getMinutes();
+    let ss = today.getSeconds();
     if(dd<10) dd='0'+dd;
     if(mm<10) mm='0'+mm;
-    today = dd+'/'+mm+'/'+yyyy+" "+hh+":"+min;
+    today = dd+'/'+mm+'/'+yyyy+" "+hh+":"+min+":"+ss;
     return today; 
+}
+
+//Container Creation:
+function createContainer(todoText,prior){
+    let newContainer = document.createElement('div')
+    newContainer.className = "todoContainer";
+    let priorDiv = document.createElement('div')
+    priorDiv.className = "priority";
+    priorDiv.innerHTML = prior;
+    let timeDiv = document.createElement('div')
+    timeDiv.className = "todoCreatedAt";
+    timeDiv.innerHTML = getTime();
+    let todoDiv = document.createElement('div')
+    todoDiv.className = "todoText";
+    todoDiv.innerHTML = todoText;
+    
+    newContainer.appendChild(priorDiv);
+    newContainer.appendChild(timeDiv);
+    newContainer.appendChild(todoDiv);
+    return newContainer;
 }
